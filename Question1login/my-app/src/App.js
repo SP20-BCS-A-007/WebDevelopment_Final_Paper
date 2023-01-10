2d9ca25abd7c6b5960196437ad31ae452aa0258e
SP20-BCS-A-007 <sp20-bcs-007@cuilahore.edu.pk>
2023-01-10 16:49:12 +0500

    1

--- Question1login/my-app/src/App.js
+++ Question1login/my-app/src/App.js
@@ -1,9 +1,28 @@
-
 import './App.css';
+import {React, useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [res, setRes] = useState({});
+  const datapost = (e)=>{
+    axios.post("https://dummyjson.com/auth/login", e )
+    .then((response) =>{
+      console.log(response);
+    })
+    .catch((error)=> {
+      console.log(error);
+    });
+  }
   return (
-    <div>HEllo</div>
+    <form>
+  <label>
+    UserName:<input type="text" name="name" />
+  </label>
+  <label>
+    Password:<input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
   );
 }
 
